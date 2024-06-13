8c755e3c80e743a0dc265510efa22d82cd7ff053
DavidAyad97 <davidayad88@gmail.com>
2024-06-13 20:07:06 +0300

    fix bug in modal of projects component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger('upDownAnimation', [
+      transition('* => *', [
+        animate('1s', style({ transform: 'translateY(-50px)' })),
+        animate('1s', style({ transform: 'translateY(50px)' }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
 

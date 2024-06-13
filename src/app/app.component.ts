af90e1dc5034d016565c989ac654c6ae9e90f7de
unknown <hossamhabeba30@gmail.com>
2024-06-13 19:55:57 +0300

    Add Footer and Pricing and Profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { UserSignupComponent } from '../components/user-signup/user-signup.component';
+import { PricingComponent } from '../components/pricing/pricing.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,UserSignupComponent],
+  imports: [RouterOutlet,NavbarComponent,UserSignupComponent, PricingComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/components/profile/profile.component.ts
+++ src/components/profile/profile.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
 import { User } from '../../interfaces/user';
 import { AuthenticationService } from '../../services/authentication.service';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
   user:User = {} as User;
+  isEditMode: boolean = false;
+  
   constructor(
     private _authService:AuthenticationService
   ){
     _authService.user.subscribe((newUser) =>{
       this.user = newUser;
-      console.log(this.user);
+     
       
     });
   }
-
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode; // Toggle the value of isEditMode
+  }
 }

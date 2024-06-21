05d39f99db63a5e4fcaf7bd44779126518321aec
DavidAyad97 <davidayad80@gmail.com>
2024-06-21 20:52:55 +0300

    add services to home

--- src/components/Core/footer/footer.component.ts
+++ src/components/Core/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../interfaces/user';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +10,12 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  user:User= {};
+  sub:Subscription = {} as Subscription;
+  constructor(private _authenticationService:AuthenticationService){}
 
+  ngOnInit(): void {
+      this._authenticationService.user.subscribe(newUser =>this.user = newUser);
+  }
 }

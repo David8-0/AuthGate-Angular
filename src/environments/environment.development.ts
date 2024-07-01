8a723c2e9ca064fda4c34d2761509febf63181ac
mahmoudmowiena <mowiena8@gmail.com>
2024-07-02 02:11:04 +0300

    env variables updates

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    domain: "https://auth-gate-backend-4bbb4e503926.herokuapp.com/",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    domain: "https://auth-gate-backend-4bbb4e503926.herokuapp.com/",
+};
